192101b06677140229bd1b6b49d0f36970a690d1
salvagariglio <salvaglinkedin@gmail.com>
2025-04-30 13:44:08 -0300

    images need more definition

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -6,117 +6,32 @@ export default function ProductosPage() {
 
     const productos = [
         {
-            brand: "KAVACO",
-            line1: "Aceite para caja",
-            line2: "Automática y manual",
-            subtitle: "",
-            imageSrc: "/images/recortes/aceite-caja-velocidad-autom-manual.png"
+            imageSrc: "/images/recortes/1.png"
         },
         {
-            brand: "ACDELCO",
-            line1: "Aceite de",
-            line2: "Motor",
-            subtitle: "",
-            imageSrc: "/images/recortes/aceite-motor-acdelco.png"
+            imageSrc: "/images/recortes/2.png"
         },
         {
-            brand: "GM ORIGINAL",
-            line1: "Kit distribución con bomba de agua y termostato",
-            line2: "",
-            subtitle: "AGILE | CORSA | COBALT | ONIX | MERIVA | PRISMA | SPIN",
-            imageSrc: "/images/recortes/kit-distribucion-bomba-de-agua-termostato-original-agile-corsa-etc.png"
+            imageSrc: "/images/recortes/3.png"
         },
         {
-            brand: "TOYOTA ORIGINAL",
-            line1: "Kit distribución original",
-            line2: "Toyota Hilux",
-            subtitle: "1KD / 2KD",
-            imageSrc: "/images/recortes/kit-distribucion-original-toyota-hilux.png"
+            imageSrc: "/images/recortes/4.png"
         },
         {
-            brand: "TOYOTA",
-            line1: "Kit distribución",
-            line2: "Toyota Hilux",
-            subtitle: "1KD / 2KD",
-            imageSrc: "/images/recortes/kit-distribucion-toyota-hilux.png"
+            imageSrc: "/images/recortes/5.png"
         },
         {
-            brand: "GM",
-            line1: "Kit distribución con bomba de agua y termostato",
-            line2: "",
-            subtitle: "AGILE | CORSA | COBALT | ONIX | MERIVA | PRISMA | SPIN",
-            imageSrc: "/images/recortes/kit-distribucion-bomba-de-agua-termostato-agile-corsa-etc.png"
+            imageSrc: "/images/recortes/6.png"
         },
         {
-            brand: "GM ORIGINAL",
-            line1: "Kit distribución",
-            line2: "",
-            subtitle: "CRUZE 1.8 16V | TRACKER 1.8 16V | SONIC 1.6 16V",
-            imageSrc: "/images/recortes/kit-distribucion-original-cruze18-tracker18-sonic-16.png"
+            imageSrc: "/images/recortes/7.png"
         },
         {
-            brand: "ACDELCO",
-            line1: "Juego de ",
-            line2: "Cables y Bujías",
-            subtitle: "CHEVROLET",
-            imageSrc: "/images/recortes/juego-de-cables-y-bujias-chevrolet.png"
+            imageSrc: "/images/recortes/8.png"
         },
         {
-            brand: "ACDELCO",
-            line1: "Amortiguador",
-            line2: "Delantero",
-            subtitle: "ORIGINAL CHEVROLET",
-            imageSrc: "/images/recortes/amortiguador-delantero-org-chevrolet.png"
+            imageSrc: "/images/recortes/9.png"
         },
-        {
-            brand: "ACDELCO",
-            line1: "Amortiguador",
-            line2: "Trasero",
-            subtitle: "ORIGINAL CHEVROLET",
-            imageSrc: "/images/recortes/amortiguador-trasero-org-chevrolet.png"
-        },
-        {
-            brand: "LUK",
-            line1: "Kit embrague",
-            line2: "S10 / Blazer 2.8TD",
-            subtitle: "MWM (258mm)",
-            imageSrc: "/images/recortes/kit-embrague-luk-s10.png"
-        },
-        {
-            brand: "ACDELCO",
-            line1: "Kit de",
-            line2: "Embrague",
-            subtitle: "ORIGINAL CHEVROLET",
-            imageSrc: "/images/recortes/kit-embrague-original-chevrolet.png"
-        },
-        {
-            brand: "GM",
-            line1: "Parrilla de",
-            line2: "Suspensión",
-            subtitle: "CRUZE 17",
-            imageSrc: "/images/recortes/parrilla-suspension-cruze-17.png"
-        },
-        {
-            brand: "GM",
-            line1: "Tapa de",
-            line2: "Distribución",
-            subtitle: "CORSA 1.4 / 1.6",
-            imageSrc: "/images/recortes/juego-tapa-distribucion-corsa-14.png"
-        },
-        {
-            brand: "MWM",
-            line1: "Kit embrague",
-            line2: "Original",
-            subtitle: "S10 / BLAZER 2.8TD MWM (258mm)",
-            imageSrc: "/images/recortes/kit-embrague-original-s10.png"
-        },
-        {
-            brand: "LUK",
-            line1: "Kit embrague",
-            line2: "Corsa 1.6",
-            subtitle: "",
-            imageSrc: "/images/recortes/kit-embrague-corsa-16.png"
-        }
     ];
 
     return (
