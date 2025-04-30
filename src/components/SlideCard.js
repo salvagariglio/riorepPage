2905765a10a9551f45eebc647fd35edb91585746
salvagariglio <salvaglinkedin@gmail.com>
2025-04-30 14:11:29 -0300

    finished

--- src/components/SlideCard.js
+++ src/components/SlideCard.js
@@ -3,18 +3,19 @@
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
-export default function SlideCard({ imageSrc }) {
+export default function SlideCard({
+    imageSrc,
+}) {
     const router = useRouter();
 
     return (
-        <article className="relative h-60 w-80 flex flex-col p-6 overflow-visible">
-            <div className="relative w-full h-full">
+        <article className="relative  h-90 w-90 flex flex-col p-6 overflow-visible">
+            <div>
                 <Image
-                    alt=""
+                    alt=''
                     src={imageSrc}
-                    fill
-                    className="object-contain"
-                    priority
+                    width={900}
+                    height={900}
                 />
             </div>
         </article>
